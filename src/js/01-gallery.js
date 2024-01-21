075ef07633511c2aabdc7d8efc52e3fa0c5b99b4
Natalia Yakovyna <yakovina.n13@gmail.com>
2024-01-21 23:03:55 +0200

    task01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox"
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+// const lightbox = new SimpleLightbox('.gallery a', { /* options */ })
+
+const galleryList = document.querySelector('.gallery');
+const galleryItemMurkup = createGalleryItemsMarkup (galleryItems);
+
+
+
+galleryList.insertAdjacentHTML('beforeend', galleryItemMurkup)
+
+// console.log(createGalleryItemsMarkup (galleryItems));
+
+function createGalleryItemsMarkup (galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+           <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+     </li>
+        `;
+    }).join('')
+    
+}
+
+const lightboxGallery = new SimpleLightbox('.gallery a', { 
+    captionsData: 'alt',
+    captionPosition:'bottom',
+    captionDelay: 250, 
+});
