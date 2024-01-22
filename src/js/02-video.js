a8fdff53fede3b793ac9c003e735ec1fd33eb300
Natalia Yakovyna <yakovina.n13@gmail.com>
2024-01-22 22:21:12 +0200

    task-02 vieo

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,40 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+console.log(iframe);
+
+const player = new Player(iframe);
+
+player.on('play', function() {
+            console.log('played the video!');
+        });
+
+player.on('timeupdate', function(data) {
+            // data is an object containing properties specific to that event
+        });
+    
+        // player.getVideoTitle().then(function(title) {
+        //     console.log('title:', title);
+        // });
+
+// const player = new Player('handstick', {
+//     id: "vimeo-player",
+//     width: 640
+// });
+
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+
+// const iframe = document.querySelector('iframe');
+// console.log(iframe);
+
+//     const player = new Vimeo.Player(iframe);
+
+//     player.on('play', function() {
+//         console.log('played the video!');
+//     });
+
+//     player.getVideoTitle().then(function(title) {
+//         console.log('title:', title);
+//     });
