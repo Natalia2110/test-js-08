c4b5d5ea2b22fe9f1415b890d3256d185aa68245
Natalia Yakovyna <yakovina.n13@gmail.com>
2024-01-24 13:33:01 +0200

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,40 +1,27 @@
 import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
-console.log(iframe);
 
 const player = new Player(iframe);
 
-player.on('play', function() {
-            console.log('played the video!');
-        });
+player.on('timeupdate', function getCurrentTime(data) {
+    localStorage.setItem('videoplayer-current-time',JSON.stringify(data));
+      });
 
-player.on('timeupdate', function(data) {
-            // data is an object containing properties specific to that event
-        });
-    
-        // player.getVideoTitle().then(function(title) {
-        //     console.log('title:', title);
-        // });
+    const currentTime = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+    console.log(currentTime);
 
-// const player = new Player('handstick', {
-//     id: "vimeo-player",
-//     width: 640
-// });
-
-// player.on('play', function() {
-//     console.log('played the video!');
-// });
-
-// const iframe = document.querySelector('iframe');
-// console.log(iframe);
-
-//     const player = new Vimeo.Player(iframe);
-
-//     player.on('play', function() {
-//         console.log('played the video!');
-//     });
-
-//     player.getVideoTitle().then(function(title) {
-//         console.log('title:', title);
-//     });
+    player.setCurrentTime(currentTime.seconds).then(function(seconds) {
+        // seconds = the actual time that the player seeked to
+        
+    }).catch(function(error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                console.log(error.name);
+                break;
+            default:
+                console.log('some other error occurred');
+                break;
+        }
+    });
